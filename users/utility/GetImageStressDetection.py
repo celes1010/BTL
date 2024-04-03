d880b6a98a091ad556db3acc4f959f0707bb1d36
celes1010 <celestineleslin7@gmail.com>
2024-04-04 02:27:13 +0530

    camera worked succesfully

--- users/utility/GetImageStressDetection.py
+++ users/utility/GetImageStressDetection.py
@@ -1,32 +1,35 @@
 from django.conf import settings
-from PyEmotion import *
+from PyEmotion import DetectFace
 import cv2 as cv
+
 class ImageExpressionDetect:
-    def getExpression(self,imagepath):
-        filepath = settings.MEDIA_ROOT + "\\" + imagepath
-        PyEmotion()
+    def getExpression(self, imagepath):
+        filepath = settings.MEDIA_ROOT + "/" + imagepath  # Use "/" instead of "\\"
         er = DetectFace(device='cpu', gpu_id=0)
-        # Open you default camera
-        # img = cv.imread('test.jpg')
-        # cap = cv.VideoCapture(0)
-        # ret, frame = cap.read()
         frame, emotion = er.predict_emotion(cv.imread(filepath))
-        cv.imshow('Alex Corporation', frame)
+        cv.imshow('InsightPlus', frame)
         cv.waitKey(0)
-        print("Hola Hi",filepath,"Emotion is ",emotion)
+        print("Hi", filepath, "Emotion is", emotion)
         return emotion
 
     def getLiveDetect(self):
         print("Streaming Started")
-        PyEmotion()
         er = DetectFace(device='cpu', gpu_id=0)
-        # Open you default camera
         cap = cv.VideoCapture(0)
-        while (True):
+        if not cap.isOpened():
+            print("Failed to open camera")
+            return
+
+        while True:
             ret, frame = cap.read()
+            if not ret:
+                print("Failed to capture frame")
+                continue
+
             frame, emotion = er.predict_emotion(frame)
             cv.imshow('Press Q to Exit', frame)
             if cv.waitKey(1) & 0xFF == ord('q'):
                 break
+
         cap.release()
         cv.destroyAllWindows()
